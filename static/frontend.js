b3ec5103bd21f07fe888c0f3907584fdfe00f4d6
Andrew Gorman <andrewgorman101@gmail.com>
2020-04-22 19:19:01 +0100

    ajax posting selected module

--- static/frontend.js
+++ static/frontend.js
@@ -1,19 +1,72 @@
-// Toggle Selection between modules
-$('.list-group-item').click(function() {
+//Add Module
+$(document).ready(function() {
+	$('.list-group-item').click(function() {
 
-    $('.active').not($(this)).removeClass('active');
-	$(this).toggleClass("active");
+	    $('.active').not($(this)).removeClass('active');
+		$(this).toggleClass("active");
 
-	let moduleID = $(this).data('moduleid');
-	console.log(moduleID)
+		let moduleID = $(this).data('moduleid');
 
-	$.ajax({
-		type: 'post',
-		url: '/module-selected',
-		data: moduleID,
-		success: function (data) {
-			console.log('posted');
-		}
+
+		//Send Selected Module ID
+		$.ajax({
+			type: 'post',
+			url: '/module-selected',
+			data: moduleID,
+			success: function (data) {
+				console.log('posted');
+			}	
+		});
 	});
+});
 
+
+//Delete Module
+$(document).ready(function() {
+    $("#remove-module").click(function(){
+
+    	$('.list-group-item').each(function(){
+	    	if ($(this).hasClass('active')) {
+		     	let moduleID = $(this).data('moduleid');
+				console.log(moduleID)
+
+		    	$.ajax({
+					type: 'post',
+					url: '/module-remove',
+					contentType: 'application/json; charset=utf-8',
+					data: JSON.stringify({'id': moduleID}),
+					success: function (data) {
+				    	$('.list-group-item').each(function(){
+	    					if ($(this).hasClass('active')) {
+	    						$(this).remove();
+	    					}
+	    				});
+					}
+				});   		
+	    	}
+	    }); 
+	});
+});
+
+//Edit Module
+$(document).ready(function() {
+    $("#edit-module").click(function(){
+
+    	$('.list-group-item').each(function(){
+	    	if ($(this).hasClass('active')) {
+		     	let moduleID = $(this).data('moduleid');
+				console.log(moduleID)
+
+		    	$.ajax({
+					type: 'post',
+					url: '/module-edit',
+					contentType: 'application/json; charset=utf-8',
+					data: JSON.stringify({'id': moduleID}),
+					success: function (data) {
+						console.log('edit posted')
+					}
+				});   		
+	    	}
+	    }); 
+	});
 });
