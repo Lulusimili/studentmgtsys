06d7618f0cc67f7cd5ccce54370b6574ad7f0c69
Andrew Gorman <andrewgorman101@gmail.com>
2020-04-22 01:00:15 +0100

    posting selected module to flask via ajax

--- static/frontend.js
+++ static/frontend.js
@@ -1,4 +1,19 @@
+// Toggle Selection between modules
 $('.list-group-item').click(function() {
+
     $('.active').not($(this)).removeClass('active');
 	$(this).toggleClass("active");
+
+	let moduleID = $(this).data('moduleid');
+	console.log(moduleID)
+
+	$.ajax({
+		type: 'post',
+		url: '/module-selected',
+		data: moduleID,
+		success: function (data) {
+			console.log('posted');
+		}
+	});
+
 });
