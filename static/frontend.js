017b719dc95a0759b9d433e8b711784cf8e80511
Andrew Gorman <andrewgorman101@gmail.com>
2020-05-02 12:55:33 +0100

    ajax dynamically updating student list

--- static/frontend.js
+++ static/frontend.js
@@ -1,30 +1,49 @@
-//Add Module
+
+// Get students for who are enrolled in selected module
 $(document).ready(function() {
 	$('.list-group-item').click(function() {
-
 	    $('.active').not($(this)).removeClass('active');
 		$(this).toggleClass("active");
 
-		let moduleID = $(this).data('moduleid');
+		$("#edit-module").removeAttr('disabled');
+		$("#remove-module").removeAttr('disabled');
 
+		let getStudentsModuleID = $(this).data('moduleid');
+		console.log(getStudentsModuleID)
 
-		//Send Selected Module ID
+		//Send Selected Module ID to fetch students enrolled in module
 		$.ajax({
 			type: 'post',
-			url: '/module-selected',
-			data: moduleID,
+			url: '/get-selected-students',
+			contentType: 'application/json; charset=utf-8',
+			data: JSON.stringify({'id': getStudentsModuleID}),
 			success: function (data) {
-				console.log('posted');
+				$('#student-list-container').load(document.URL +  ' #student-list');
+
 			}	
 		});
 	});
 });
 
 
+//Add Module
+$('#add-module').click(function() {
+	$('#addModuleForm').attr('action', '/add_module');
+	$('#moduleID').removeAttr('disabled');
+	$('#moduleID').val('');
+	$('#moduleName').val('');
+	$('#moduleLecturer').val('');
+	$('#moduleCapacity').val('');
+	$('#modal-submit-btn').html('Add Module');
+	$('#staticBackdropLabel').html('Add Module');
+}); 
+
+
 //Delete Module
 $(document).ready(function() {
     $("#remove-module").click(function(){
-
+    	$("#remove-module").attr('disabled', 'disabled');
+    	$("#edit-module").attr('disabled', 'disabled');
     	$('.list-group-item').each(function(){
 	    	if ($(this).hasClass('active')) {
 		     	let moduleID = $(this).data('moduleid');
@@ -37,7 +56,7 @@ $(document).ready(function() {
 					data: JSON.stringify({'id': moduleID}),
 					success: function (data) {
 				    	$('.list-group-item').each(function(){
-	    					if ($(this).hasClass('active')) {
+	    					if ($(this).hasClass('active')) { 
 	    						$(this).remove();
 	    					}
 	    				});
@@ -52,21 +71,25 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#edit-module").click(function(){
 
+    	$('#addModuleForm').attr('action', '/module_edit');
+    	$('#moduleID').attr('readonly', 'readonly');
+		$('#modal-submit-btn').html('Edit Module');
+		$('#staticBackdropLabel').html('Edit Module');	
     	$('.list-group-item').each(function(){
 	    	if ($(this).hasClass('active')) {
-		     	let moduleID = $(this).data('moduleid');
-				console.log(moduleID)
+		     	let editModuleID = $(this).data('moduleid');
+				console.log(editModuleID)
 
 		    	$.ajax({
 					type: 'post',
-					url: '/module-edit',
+					url: '/select-module-edit',
 					contentType: 'application/json; charset=utf-8',
-					data: JSON.stringify({'id': moduleID}),
+					data: JSON.stringify({'id': editModuleID}),
 					success: function (data) {
-						$('#moduleID').val(moduleID);
+						$('#moduleID').val(editModuleID);
 						$('#moduleName').val(data[0]);
 						$('#moduleLecturer').val(data[1]);
-						$('#moduleCapacity').val(data[2]);
+						$('#moduleCapacity').val(data[2]);				
 					}
 				});   		
 	    	}
