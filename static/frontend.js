0fc12943710c769a0b8f86f611ffef3b8afcaa94
Andrew Gorman <andrewgorman101@gmail.com>
2020-05-02 22:08:42 +0100

    css styling and full list of students populating:

--- static/frontend.js
+++ static/frontend.js
@@ -1,15 +1,95 @@
 
+// Navigation Bar
+$(document).ready(function() {
+	$('#viewModules').click(function() {
+		$('#viewStudents').removeClass('active');
+		$(this).addClass('active');
+		$('#module-header').show();
+		$('#student-list-container').hide();
+		$('#module_container').css('min-height', 'calc(100vh - 450px)');
+	});
+});
+
+$(document).ready(function() {
+	$('#dashboard').click(function() {
+		location.reload();
+	});
+});
+
+
+$(document).ready(function() {
+	$('#viewStudents').click(function() {
+		$('#viewModules').removeClass('active');
+		$(this).addClass('active');
+		$('#module-header').hide();
+		$('#student-list-container').show();
+		$('#student-btn').html('Register New Student');
+
+		$('.fullStudentList').each(function() {
+			$(this).show();
+		});
+		
+
+		$('.studentEnrollmentList').hide();
+		$('.fullStudentList').css('display', 'all');
+
+
+
+		$('.student-option-btn').each(function() {
+			$(this).html('Edit Student');
+		});
+
+	});
+});
+
+
+
+//Selected Student on Student List
+$(document).on('click', '.studentEnrollmentList', function(e) { 
+	
+	$('.studentEnrollmentList').not($(this)).removeClass('activeStudentRow');
+	$(this).addClass("activeStudentRow");
+	selectedStudentID = $(this).attr('data');
+	$('.student-option-btn').attr('disabled', 'disabled');
+	$(this).find("button").removeAttr('disabled');
+	$('.student-option-btn').css('background-color', '#6c757d');
+	$(this).find("button").css('background-color', 'orange');
+
+});
+
+//Selected Student on Student List
+$(document).on('click', '.fullStudentList', function(e) { 
+	
+	$('.fullStudentList').not($(this)).removeClass('activeStudentRow');
+	$(this).addClass("activeStudentRow");
+	selectedStudentID = $(this).attr('data');
+	$('.student-option-btn').attr('disabled', 'disabled');
+	$(this).find("button").removeAttr('disabled');
+	$('.student-option-btn').css('background-color', '#6c757d');
+	$(this).find("button").css('background-color', 'orange');
+
+});
+
+
 // Get students for who are enrolled in selected module
 $(document).ready(function() {
 	$('.list-group-item').click(function() {
 	    $('.active').not($(this)).removeClass('active');
-		$(this).toggleClass("active");
+		$(this).addClass("active");
 
 		$("#edit-module").removeAttr('disabled');
 		$("#remove-module").removeAttr('disabled');
 
+		$('#student-btn').html('Add Student');
+		$('#student-option-btn').html('Remove');
+
+
+		$('#module_container').css('min-height', '420px');
+		$('#student-list-container').show();
+
 		let getStudentsModuleID = $(this).data('moduleid');
 		console.log(getStudentsModuleID)
+		$('#small-moduleID').html(getStudentsModuleID);
 
 		//Send Selected Module ID to fetch students enrolled in module
 		$.ajax({
@@ -19,7 +99,6 @@ $(document).ready(function() {
 			data: JSON.stringify({'id': getStudentsModuleID}),
 			success: function (data) {
 				$('#student-list-container').load(document.URL +  ' #student-list');
-
 			}	
 		});
 	});
@@ -58,6 +137,12 @@ $(document).ready(function() {
 				    	$('.list-group-item').each(function(){
 	    					if ($(this).hasClass('active')) { 
 	    						$(this).remove();
+	    						$('.list-group-item').each(function(index){
+    							    if (index === $('.list-group-item').length - 1) {
+		    							$(this).trigger('click');
+		    							$(this).addClass('active');
+									}
+	    						});
 	    					}
 	    				});
 					}
@@ -89,7 +174,8 @@ $(document).ready(function() {
 						$('#moduleID').val(editModuleID);
 						$('#moduleName').val(data[0]);
 						$('#moduleLecturer').val(data[1]);
-						$('#moduleCapacity').val(data[2]);				
+						$('#moduleCapacity').val(data[2]);
+
 					}
 				});   		
 	    	}
